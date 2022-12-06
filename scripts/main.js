ac04ecdcc9b05d80916d941a0ec1fcdf2597a5da
swardinfo <sward1000@gmail.com>
2022-12-06 21:33:59 +1000

    Nav menu now closes when it or document clicked

--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,69 @@ const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
 const mediaQuery = window.matchMedia("(min-width: 1000px)");
 
+
+/**
+ * Adds/remove class indicator to html indicating if JavaScript should
+ * be used based on viewport size.
+ * @param {document#event:screenSizeChange} e
+ */
 function handleScreenSizeChange(e) {
     var html = document.getElementsByTagName("html")[0];
     if (e.matches) {
         html.classList.remove("js");
         menuButton.setAttribute("aria-disabled", true);
-        menu.classList.remove("menu-open");
-    } else {
+        closeMenu();
+        return;
+    }
         html.classList.add("js");
         menuButton.setAttribute("aria-disabled", false);
-    }
 }
 
-// Adds/removes a class to the menu ul so CSS can open/close the Nav menu
+/**
+ * Handles the menuButton click event to toggle the navigation menu open
+ */
 menuButton.addEventListener("click", () => {
-    var openMenu = menu.classList.contains("menu-open");
-    menuButton.setAttribute("aria-expanded", !openMenu);
-    menu.classList.toggle("menu-open");
+    if (menu.classList.contains("menu-open")) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
 });
 
+/**
+ * Opens the navigation menu.
+ */
+function openMenu() {
+    menuButton.setAttribute("aria-expanded", true);
+    menu.classList.add("menu-open");
+}
+
+/**
+ * Closes the navigation menu.
+ */
+function closeMenu() {
+    menuButton.setAttribute("aria-expanded", false);
+    menu.classList.remove("menu-open");
+}
+
+/**
+ * Adds an event to monitor screen size changes
+ */
 mediaQuery.addListener(handleScreenSizeChange);
 
-// Adds class 'js' to html tag to detect if JavaScript is present for CSS
+/**
+ * Handles the ScreenSizeChange event.
+ */
 window.addEventListener('load', function() {
     handleScreenSizeChange(mediaQuery);
 }, {once: true});
+
+/**
+ * Handles the document click event.
+ */
+document.addEventListener("click", function(e)  {
+    if (e.target.id === "menu" || e.target === menuButton ) {
+        return;
+    }
+    closeMenu();
+});
